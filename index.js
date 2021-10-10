57d9281a53e3eff5467d003e06e913128fbbe11d
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-10-10 08:33:02 -0500

    fixed duplication issue, had to delete connection js in order to work

--- index.js
+++ index.js
@@ -2,36 +2,48 @@ const express = require("express");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const promisemysql = require("promise-mysql");
-const db = require("./db/connection");
+require("dotenv").config();
+const mysql = require("mysql2");
 
+const connectionProperties = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+});
+
+connectionProperties.connect((err) => {
+  if (err) throw err;
+
+  //Start menu main function once initiated
+  console.log("\n You are now connected to the Workforce Database.\n");
+  iQ();
+});
 // iQ = initial Question(main menu), general setup for startup in the database once sql is initiated
 function iQ() {
   inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "iQ",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Employee By Department",
-          "View All Employee by Manager",
-          "Add Employee",
-          "Remove Employee",
-          "Update Employee",
-          "Update Employee's Role",
-          "Update Employee's Manager",
-          "View All Departments",
-          "View Department Budget",
-          "Add Department",
-          "Remove Department",
-          "View All Roles",
-          "Add Role",
-          "Remove Role",
-          "Quit",
-        ],
-      },
-    ])
+    .prompt({
+      name: "action",
+      type: "list",
+      name: "iQ",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employee By Department",
+        "View All Employee by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee",
+        "Update Employee's Manager",
+        "Add Department",
+        "Remove Department",
+        "View All Roles",
+        "Add Role",
+        "Remove Role",
+        "Quit",
+      ],
+    })
     .then((dataAnswer) => {
       switch (dataAnswer.action) {
         case "View All Employees":
@@ -62,7 +74,7 @@ function iQ() {
           //uER = Update Employee's Role
           uER();
           break;
-        case "Update Employee's Manager":
+        case "Update Employee's Manager1":
           //uEM = Update Employee's Manager
           uEM();
           break;
